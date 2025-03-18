0aa045d7d6351941bad1e00602f387358faff362
cetmoko <gayatrimm17@gmail.com>
2025-03-18 13:37:41 -0400

    added files, created buttons and basic operation functions

---
+++ calculator.js
@@ -0,0 +1,26 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function(a, b, operator) {
+}
+
+// getting data from clicks
+
+
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
