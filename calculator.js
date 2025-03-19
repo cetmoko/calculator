93d00fabc18cffbeb03edf8220a30925175ac4dd
cetmoko <gayatrimm17@gmail.com>
2025-03-19 16:11:17 -0400

    buttons work now, yet to implement display

--- calculator.js
+++ calculator.js
@@ -1,26 +1,74 @@
-const add = function(a, b) {
-    return a + b;
-}
+const container = document.querySelector('#container');
 
-const subtract = function(a, b) {
-    return a - b;
-}
-
-const multiply = function(a, b) {
-    return a * b;
-}
-
-const divide = function(a, b) {
-    return a / b;
-}
+const calculator = {a: "", b: "", operator: "", display: "", op_change: false};
 
 const operate = function(a, b, operator) {
+    switch(operator) {
+        case "+":
+            return a+b;
+        case "-":
+            return a-b;
+        case "*":
+            return a*b;
+        case "/":
+            if (b === 0) {
+                return "wtf are you doing bruh?? press clear to continue";
+            } else {
+                return a/b;
+            };
+    }
 }
 
-// getting data from clicks
+const get_from_display = function(input) {
+    if (([0,1,2,3,4,5,6,7,8,9].includes(+input)) && (calculator.op_change === false)) {
+        calculator.display += input;
+        calculator.a += input;
+    }
+    else if (([0,1,2,3,4,5,6,7,8,9].includes(+input)) && (calculator.op_change === true)) {
+        calculator.display += input;
+        calculator.b += input;
+    }
+    else if ((["+", "-", "*", "/"].includes(input)) && (calculator.op_change === false)) {
+        calculator.b = "";
+        calculator.display += input;
+        calculator.operator = input;
+        calculator.op_change = true;
+    }
+    else if (input === "=") {
+        calculator.display = operate(+calculator.a, +calculator.b, calculator.operator);
+        calculator.operator = "";
+        calculator.op_change = false;
+        calculator.a = calculator.display;
+    }
+    else if ((["+", "-", "*", "/"].includes(input)) && (calculator.op_change === true)) {
+        calculator.display = operate(+calculator.a, +calculator.b, calculator.operator);
+        calculator.a = calculator.display;
+        calculator.display += input;
+        calculator.operator = input;
+        calculator.b = "";
+        calculator.op_change = true;
+    }
+    else if (input === "clear") {
+        calculator.a = "";
+        calculator.b = "";
+        calculator.display = "";
+        calculator.operator = "";
+        calculator.op_change = false;
+    }
+}
 
+// button functionality
+for (i = 0; i < 10; i++) {
+    const numButton = document.createElement('button');
+    numButton.textContent = i;
+    numButton.addEventListener('click', function() {get_from_display(numButton.textContent); console.log(calculator);})
+    container.append(numButton)
+}
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+operations = {"+": "+", "-": "-", "×": "*", "÷": "/", "=": "=", "CLEAR": "clear"}
+for (const key in operations) {
+    const opButton = document.createElement('button');
+    opButton.textContent = key;
+    opButton.addEventListener('click', function() {get_from_display(operations[key]); console.log(calculator);})
+    container.append(opButton)
+}
